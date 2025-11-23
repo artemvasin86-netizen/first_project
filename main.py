cca54203331f70d4dfdaa854883a4c1adff28385
Artem Vasin <artem.vasin86@yandex.ru>
2025-11-23 17:18:17 +0300

    $ git commit -m

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('Hello from repository!!!')
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def print_author():
+    author = os.getenv("AUTHOR")
+    if author:
+        print(f"Автор проекта: {author}")
+    else:
+        print("Автор проекта не установлен в .env")
+
+# Проверка работы функции
+if __name__ == "__main__":
+    print_author()
